a8e798134e714e2959bd70e297066e3069d18433
Dave Naugler <davenaugler@gmail.com>
2023-08-10 15:51:23 -0600

    Initial Commit for Assignment #5

---
+++ src/com/coderscampus/Assignment5/CustomListApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = new ArrayList<>();
+		
+	}
+
+}
