6cc8196f91ada43243ecb6c2c5d7cd90f19e4718
Dave Naugler <davenaugler@gmail.com>
2023-08-10 16:27:30 -0600

    Notes added to class files

--- src/com/coderscampus/Assignment5/CustomArrayList.java
+++ src/com/coderscampus/Assignment5/CustomArrayList.java
@@ -1,24 +1,28 @@
 package com.coderscampus.Assignment5;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	
+	// backed by a real Java array
+	// When you add more than 10 elements it should 
+	// increase in size automatically
+	// When adding a new element, if the backing Object array is full, 
+	// you should have the array double in size.
 	Object[] items = new Object[10];
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
+		// fill this with the content required to successfully add
 		return false;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
+		// fill this with the content required to getSize();
 		return 0;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
+		// fill this with the content required to get();
 		return null;
 	}
 
--- src/com/coderscampus/Assignment5/CustomListApplication.java
+++ src/com/coderscampus/Assignment5/CustomListApplication.java
@@ -1,12 +1,15 @@
 package com.coderscampus.Assignment5;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CustomListApplication {
 
 	public static void main(String[] args) {
-		List<Integer> numbers = new ArrayList<>();
+		CustomList<String> myCustomList = new CustomArrayList<>();
+		myCustomList.add("element 1"); // and continue to add another 10, 20 or 40 more elements
+		
+		// then you should validate that all the elements you've inserted actually exist in your data structure
+		for (int i=0; i<myCustomList.getSize(); i++) {
+		    System.out.println(myCustomList.get(i));
+		}
 		
 	}
 
