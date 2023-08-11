46e6fcd7a7bbe8f11a1ef605a6b4814c4a285902
Dave Naugler <davenaugler@gmail.com>
2023-08-10 22:02:39 -0600

    updated code and comments for submission

--- src/com/coderscampus/Assignment5/CustomArrayList.java
+++ src/com/coderscampus/Assignment5/CustomArrayList.java
@@ -6,42 +6,33 @@ public class CustomArrayList<T> implements CustomList<T> {
 	private int defaultCapacity = 10;
 	private Object[] items;
 	private int size;
-	
+
 	public CustomArrayList() {
 		items = new Object[defaultCapacity];
 		size = 0;
 	}
-	
-	
-	// Back'd by a real Java array
-	// When you add more than 10 elements it should 
-	// increase in size automatically
-	// When adding a new element, if the backing Object array is full, 
-	// you should have the array double in size.
-//	Object[] items = new Object[10];
-
 
 	@Override
 	public boolean add(T item) {
 		checkCapacity(size + 1);
 		items[size++] = item;
-		return false; // or true?
+		return true;
 	}
-	
+
 	private void checkCapacity(int minCapacity) {
 		int oldCapacity = items.length;
 		if (minCapacity > oldCapacity) {
-			int newCapacity = oldCapacity * 2; 
+			int newCapacity = oldCapacity * 2;
 			items = Arrays.copyOf(items, newCapacity);
 			System.out.println("New Capacity: " + newCapacity);
 		}
 	}
-	
+
 	@Override
 	public int getSize() {
 		return size;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
@@ -51,49 +42,31 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return (T) items[index];
 	}
 	
-
-		
-		
-		
-		
-		
-		// CHECK CAPACITY
-		// When an item is added, check to see if there is enough space
-		// If not double the size, from 10
-		// If there is enough space add the item
-		
-		// RESIZING
-		// If the current capacity isn't big enough, the ArrayList
-		// will create an array with a larger capacity, double the current size
-		// and then copy the existing elements from the old array to the new array
-		
-		// ADDING ELEMENT
-		// Once there is enough space, the new element is placed in the next
-		// position in the ArrayList. The ArrayList keeps track of the number 
-		// of elements it contains and updates this count accordingly
-		
-		// UPDATE SIZE 
-		// After adding the element, the size of the ArrayList is increased by
-		// one to reflect the addition of the new element
-//		System.out.println(item);
-		// fill this with the content required to successfully add
 	
 
-//	@Override
-//	public int getSize() {
-//		 fill this with the content required to getSize();
-//		return 0;
-//	}
-//
-//	@Override
-//	public T get(int index) {
-//		 fill this with the content required to get();
-//		return null;
-//	}
-	
-	// REMOVE
+	// CHECK LIST
+
+	// [x] - CHECK CAPACITY: in checkCapacity method
+	// When an item is added, check to see if there is enough space
+	// If not double the size, from 10
+	// If there is enough space add the item
+
+	// [x] - RESIZING: in checkCapacity method (maybe another method for this?)
+	// If the current capacity isn't big enough, the ArrayList
+	// will create an array with a larger capacity, double the current size
+	// and then copy the existing elements from the old array to the new array
+
+	// [x] - ADDING ELEMENT: in add method
+	// Once there is enough space, the new element is placed in the next
+	// position in the ArrayList. The ArrayList keeps track of the number
+	// of elements it contains and updates this count accordingly
+
+	// [x] - UPDATE SIZE: in add method
+	// After adding the element, the size of the ArrayList is increased by
+	// one to reflect the addition of the new element
+
+	// REMOVE - Coming soon
 	// Will need to remove an element down the line
 	// Think about how you plan on making that happen
-	
 
 }
--- src/com/coderscampus/Assignment5/CustomListApplication.java
+++ src/com/coderscampus/Assignment5/CustomListApplication.java
@@ -3,66 +3,19 @@ package com.coderscampus.Assignment5;
 public class CustomListApplication {
 
 	public static void main(String[] args) {
-		CustomList<String> myCustomList = new CustomArrayList<>();
-		myCustomList.add("element 1"); // and continue to add another 10, 20 or 40 more elements
-		myCustomList.add("element 2"); 
-		myCustomList.add("element 3"); 
-		myCustomList.add("element 4"); 
-		myCustomList.add("element 5"); 
-		myCustomList.add("element 6"); 
-		myCustomList.add("element 7"); 
-		myCustomList.add("element 8"); 
-		myCustomList.add("element 9"); 
-		myCustomList.add("element 10"); 
-		myCustomList.add("element 11");
-		myCustomList.add("element 12"); 
-		myCustomList.add("element 13"); 
-		myCustomList.add("element 14"); 
-		myCustomList.add("element 15"); 
-		myCustomList.add("element 16"); 
-		myCustomList.add("element 17"); 
-		myCustomList.add("element 18"); 
-		myCustomList.add("element 19"); 
-		myCustomList.add("element 20"); 
-		myCustomList.add("element 21"); 
-		myCustomList.add("element 22"); 
-		myCustomList.add("element 23"); 
-		myCustomList.add("element 24");
-		myCustomList.add("element 25"); 
-		myCustomList.add("element 26"); 
-		myCustomList.add("element 27"); 
-		myCustomList.add("element 28"); 
-		myCustomList.add("element 29"); 
-		myCustomList.add("element 30"); 
-		myCustomList.add("element 31"); 
-		myCustomList.add("element 32"); 
-		myCustomList.add("element 33"); 
-		myCustomList.add("element 34"); 
-		myCustomList.add("element 35"); 
-		myCustomList.add("element 36"); 
-		myCustomList.add("element 37"); 
-		myCustomList.add("element 38"); 
-		myCustomList.add("element 39"); 
-		myCustomList.add("element 40"); 
-		myCustomList.add("element 41"); 
-//		myCustomList.add("element 42"); 
-//		myCustomList.add("element 43"); 
-//		myCustomList.add("element 44"); 
-//		myCustomList.add("element 45"); 
-//		myCustomList.add("element 46"); 
-//		myCustomList.add("element 47"); 
-//		myCustomList.add("element 48"); 
-//		myCustomList.add("element 49"); 
-//		myCustomList.add("element 50"); 
+		CustomList<Integer> myCustomList = new CustomArrayList<>();
+//		myCustomList.add("element 1");  and continue to add another 10, 20 or 40 more elements
+		
+		for (int i = 1; i < 101; i++) {
+			myCustomList.add(i);
+		}
 		
 		System.out.println("getSize: " + myCustomList.getSize());
-//		myCustomList.getSize();
 		System.out.println("get: " + myCustomList.get(0));
-//		myCustomList.get(0);
-		
+		System.out.println("--------");
 		// then you should validate that all the elements you've inserted actually exist in your data structure
 		for (int i=0; i<myCustomList.getSize(); i++) {
-		    System.out.println("Check check: " + myCustomList.get(i));
+		    System.out.println(myCustomList.get(i));
 		}
 		
 		
